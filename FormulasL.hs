be7032cc46d60c7ea1104cab6ac954a66e3bb5ba
Aleksandra <33431481+aczyz@users.noreply.github.com>
2017-12-01 12:52:16 +0100

    Graph and Formulas updated (#3)
    * Pliki z ostatniego spotkania
    
    * Formulas edit, Operator added
    
    * graphCreator added
    
    * updated Formulas, Graph with use of Data.Graph
    
    * negP added

--- FormulasL.hs
+++ FormulasL.hs
@@ -1,48 +1,47 @@
 module Formulas where
 
-data Atom = A Int | N Atom
-            deriving (Show, Eq)
+import Data.List
+data Atom = A Int
+            deriving (Show)
+
+instance Eq Atom where
+        A a == A b = a == b
 
 type HClause = (Atom, [Atom], [Atom])
 type LogicP = [HClause]
 
-hClHead :: HClause -> [Atom]            -- returns head of horn clause
+-- returns head of horn clause
+hClHead :: HClause -> [Atom]            
 hClHead (x, _, _) = [x]
 
-hClBody :: HClause -> [Atom]            -- returns body of horn clause
+-- returns body of horn clause
+hClBody :: HClause -> [Atom]            
 hClBody (_, [], []) = []
 hClBody (_, xs, ys) = xs ++ ys
 
-hClBodyP :: HClause -> [Atom]           -- returns positive body of horn clause
+
+-- returns positive body of horn clause
+hClBodyP :: HClause -> [Atom]           
 hClBodyP (_, [], _) = []
 hClBodyP (_, xs, _) = xs
 
-hClBodyN :: HClause -> [Atom]           -- returns negative body of horn clause
+
+-- returns negative body of horn clause
+hClBodyN :: HClause -> [Atom]           
 hClBodyN (_, _, [])  = []
 hClBodyN (_, _, ys) = ys
 
-turnNtoP :: [Atom] -> [Atom]            -- changes sth negative to positive
-turnNtoP []     = []
-turnNtoP (x:xs) = case x of 
-                        N a -> [a] ++ turnNtoP xs
-                        _   -> [x] ++ turnNtoP xs
-
-bPHead :: LogicP -> [Atom]              -- returns heads of logic program
+-- returns all heads of logic program
+bPHead :: LogicP -> [Atom]              
 bPHead []     = []
-bPHead (x:xs) = case hClHead x of 
-                        [N a] -> a : bPHead xs
-                        _     -> hClHead x ++ bPHead xs
+bPHead (x:xs) = hClHead x ++ bPHead xs
 
-bPBody :: LogicP -> [Atom]              -- returns bodies of logic program
+-- returns bodies of logic program
+bPBody :: LogicP -> [Atom]              
 bPBody []     = []
-bPBody (x:xs) = hClBodyP x ++ turnNtoP (hClBodyN x) ++ bPBody xs
+bPBody (x:xs) = hClBodyP x ++ hClBodyN x ++ bPBody xs
 
-rmdups :: Eq a => [a] -> [a]            -- removes duplicates
-rmdups []     = []
-rmdups (x:xs) = if   x `elem` xs 
-                then rmdups xs 
-                else x : rmdups xs
-
-bP :: LogicP -> [Atom]                  -- returns herbrand base of logic program (without duplicates)
+-- returns herbrand base of logic program (without duplicates)
+bP :: LogicP -> [Atom]                 
 bP [] = []
-bP xs = rmdups (bPHead xs ++ bPBody xs)
+bP xs = nub (bPHead xs ++ bPBody xs)
