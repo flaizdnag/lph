bd46f1deff13b25326fcc8e85132a3552b75055f
kingaord <33434900+kingaord@users.noreply.github.com>
2017-12-07 21:20:13 +0100

    Updated CCompletion
    Added negP', logicP', comp

--- CCompletion.hs
+++ CCompletion.hs
@@ -30,31 +30,31 @@ logicP' xs = [x | x <- xs, isElem (hClHead x) (negP' xs)]
 -- changes negative atoms to N Form
 addN :: [Atom] -> [Form]
 addN x = case x of 
-  []     -> []
-  (y:ys) -> N (V y) : addN ys
+              []     -> []
+              (y:ys) -> N (V y) : addN ys
 
 -- changes positive atoms to V Atom
 atomToForm :: [Atom] -> [Form]
 atomToForm x = case x of
-        []     -> []
-        (y:ys) -> V y : atomToForm ys
+                    []     -> []
+                    (y:ys) -> V y : atomToForm ys
 
 -- connects elements of the horn clauses body by conjunction
 bodyToC :: HClause -> Form
 bodyToC x = case x of 
 {-
-     (_, [], neg)  -> case neg of
-                           n -> N (V n)
-                           (n:ns) -> C (addN (n:ns))
-     (_, pos, [])  -> case pos of
-                           p -> V p
-                           (p:ps) -> C (atomToForm (p:ps)) ??? -}
-     (_, pos, neg) -> C (atomToForm pos ++ addN neg)
+                (_, [], neg)  -> case neg of
+                                      n -> N (V n)
+                                      (n:ns) -> C (addN (n:ns))
+                (_, pos, [])  -> case pos of
+                                      p -> V p
+                                      (p:ps) -> C (atomToForm (p:ps)) ??? -}
+                (_, pos, neg) -> C (atomToForm pos ++ addN neg)
 
 -- connects head and body of horn clause by equivalence
 hClToEq :: HClause -> Form
 hClToEq x = case x of 
-     (h, _, _) -> E (V h) (bodyToC x) 
+                 (h, _, _) -> E (V h) (bodyToC x) 
 
 comp :: LogicP -> [Form]
 comp []     = []
