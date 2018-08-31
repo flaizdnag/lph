3d306f7098d3e2ffbaf724cd8feba928440020f1
flaizdnag <andrzej.m.gajda@gmail.com>
2018-08-31 13:15:50 +0200

    v1.0

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Main where
 
+import Formulas
+import Completion
+import Acceptable
+import Web.Scotty               (scotty, post, body, text, ActionM)
+import Data.Text.Lazy           (Text, pack, unpack)
+import Data.Text.Lazy.Encoding  (decodeUtf8)
 
 main :: IO ()
-main = putStrLn "LPH"
+main = scotty 10100 $ do
+    
+    post "/api/bP" $ do
+        b <- decodeUtf8 <$> body
+        responseLP b bP
+
+    post "/api/positiveBodies" $ do
+        b <- decodeUtf8 <$> body
+        responseLP b bPBodiesP
+
+    post "/api/negativeBodies" $ do
+        b <- decodeUtf8 <$> body
+        responseLP b bPBodiesN
+
+    post "/api/completion" $ do
+        b <- decodeUtf8 <$> body
+        responseLP b comp
+
+    post "/api/isAcceptable" $ do
+        b <- decodeUtf8 <$> body
+        responseLP b isAcceptable
+
+responseLP :: Show a => Text -> (LogicP -> a) -> Web.Scotty.ActionM ()
+responseLP x f = textPackShow $ f $ readUnpackLP x
+    where
+        readUnpackLP = \x -> read (unpack x) :: LogicP
+        textPackShow = \x -> text $ pack $ show x
