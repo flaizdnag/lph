c99debba41cba04f3e37acf9e76d5df73afe8d48
Aleksandra <33431481+aczyz@users.noreply.github.com>
2018-12-03 12:54:28 +0100

    Tests updated with new types (#20)
    * Pliki z ostatniego spotkania
    
    * Formulas edit, Operator added
    
    * graphCreator added
    
    * updated Formulas, Graph with use of Data.Graph
    
    * negP added
    
    * Operator modified
    
    * changed files names
    
    * Completion and Operator modified, added Examples
    
    * single change in Operator
    
    * level mapping added
    
    * level mapping and addD modified
    
    * groupBy added, addC modified
    
    * added disjunction
    
    * equivalence added
    
    * grouping by value added
    
    * trueC/D/E modified
    
    * changes in CPL part
    
    * modified Completion, separated Level Mapping
    
    * project bulit, changes in Completion and Examples
    
    * deleting dispensable files
    
    * Delete LvlMap.hs
    
    * Delete Completion.hs
    
    * Delete Examples.hs
    
    * Delete stack.yaml
    
    * Changes in tests
    
    * Test updated

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,54 +1,53 @@
-import Formulas
-import Operator
-import Completion
+import LogicPrograms
+import TpOperator
 import Test.Hspec
 
-ex1 :: LogicP
-ex1 = [ (A 1 [], [A 2 [], A 3 []], [A 4 []]),
-        (A 5 [], [A 6 []], []),
-        (A 10 [], [], []),
-        (A 1 [], [], [])]
+ex1 :: LP
+ex1 = [ (Cl (A 1 "") [A 2 "", A 3 ""] [A 4 ""]),
+        (Cl (A 5 "") [A 6 ""] []),
+        (Cl (A 10 "") [] []),
+        (Cl (A 1 "") [] [])]
 
 main :: IO ()
 main = hspec $ do
-    describe "Tests for funcions in Formulas module" $ do
-        it "hClHead returns head of Horn clause in a list" $
-            hClHead (A 1 [], [], []) `shouldBe` A 1 []
+    describe "Tests for funcions in LogicPrograms module" $ do
+        it "clHead returns head of Horn clause in a list" $
+            clHead (Cl (A 1 "") [] []) `shouldBe` A 1 ""
 
-        it "hClBody returns body of a Horn clause without repetitions" $
-            hClBody (A 1 [], [A 1 [], A 2 []], [A 2 []]) `shouldBe` [A 1 [], A 2 []]
+        it "clBody returns body of a Horn clause without repetitions" $
+            clBody (Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""]) `shouldBe` [A 1 "", A 2 ""]
 
-        it "hClBodyP returns positive body of a Horn clause without repetitions" $
-            hClBodyP (A 1 [], [A 1 [], A 2 []], [A 2 []]) `shouldBe` [A 1 [], A 2 []]
+        it "clPBody returns positive body of a Horn clause without repetitions" $
+            clPBody (Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""]) `shouldBe` [A 1 "", A 2 ""]
 
-        it "hClBodyN returns positive body of a Horn clause without repetitions" $
-            hClBodyN (A 1 [], [A 1 [], A 2 []], [A 2 []]) `shouldBe` [A 2 []]
+        it "clNBody returns negative body of a Horn clause without repetitions" $
+            clNBody (Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""]) `shouldBe` [A 2 ""]
 
-        it "bPHead returns all heads from a given LP without repetitions" $
-            bPHeads [(A 1 [], [A 1 [], A 2 []], [A 2 []]), (A 1 [], [], [])] `shouldBe` [A 1 []]
+        it "lpHeads returns all heads from a given LP without repetitions" $
+            lpHeads [Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""], Cl (A 1 "") [] []] `shouldBe` [A 1 ""]
 
-        it "bPBody returns all atoms from the bodies of Horn clauses from a given LP without repetitions" $
-            bPBodies [(A 1 [], [A 1 [], A 2 []], [A 2 []]), (A 1 [], [A 1 []], [])] `shouldBe` [A 1 [], A 2 []]
+        it "lpBodies returns all atoms from the bodies of Horn clauses from a given LP without repetitions" $
+            lpBodies [Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""], Cl (A 1 "") [A 1 ""] []] `shouldBe` [A 1 "", A 2 ""]
 
-        it "bPBodyP returns all atoms from the positive bodies of Horn clauses from a given LP without repetitions" $
-            bPBodiesP [(A 1 [], [A 1 [], A 2 []], [A 2 []]), (A 1 [], [A 1 []], [])] `shouldBe` [A 1 [], A 2 []]
+        it "lpPBodies returns all atoms from the positive bodies of Horn clauses from a given LP without repetitions" $
+            lpPBodies [Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""], Cl (A 1 "") [A 1 ""] []] `shouldBe` [A 1 "", A 2 ""]
 
-        it "bP returns all atoms occuring in a given LP without repetitions" $
-            bP [(A 1 [], [A 1 [], A 2 []], [A 2 []]), (A 1 [], [A 1 []], [])] `shouldBe` [A 1 [], A 2 []]
+        it "bp returns all atoms occuring in a given LP without repetitions" $
+            bp [Cl (A 1 "") [A 1 "", A 2 ""] [A 2 ""], Cl (A 1 "") [A 1 ""] []] `shouldBe` [A 1 "", A 2 ""]
 
-    describe "Tests for funcions in Operator module" $ do
+    describe "Tests for funcions in TpOperator module" $ do
         it "opTp returns head of a clause that positive body is a subset of a list of atoms without duplicates" $
-            opTp [(A 1 [], [A 1 [], A 2 []], [A 3 []]), (A 1 [], [A 1 []], [])] [A 1 [], A 2 []] `shouldBe` [A 1 []]
+            opTp ([Cl (A 1 "") [A 1 "", A 2 ""] [A 3 ""], Cl (A 1 "") [A 1 ""] []]) (IntLP [A 1 "", A 2 ""] [A 3 ""]) `shouldBe` IntLP [A 1 ""] [A 2 "", A 3 ""]
 
         it "opTp returns head of a clause that positive body is a subset of a list of atoms without duplicates" $
-            opTp [(A 1 [], [A 1 [], A 2 []], [A 3 []]), (A 1 [], [A 1 []], [])] [A 1 [], A 2 []] `shouldBe` [A 1 []]
+            opTp ([Cl (A 1 "") [A 1 "", A 2 ""] [A 3 ""], Cl (A 1 "") [A 1 ""] []]) (IntLP [] []) `shouldBe` IntLP [] [A 1 "", A 2 "", A 3 ""]
 
         it "upArrow returns model for acceptable LP (first item)" $
-            head (upArrow [(A 1 [], [A 2 []], []), (A 1 [], [A 3 []], []), (A 3 [], [], []), (A 2 [], [], [])]) `shouldBe` [A 1 [], A 3 [], A 2 []]
+            head (upArrow [Cl (A 1 "") [A 2 ""] [], Cl (A 1 "") [A 3 ""] [], Cl (A 3 "") [] [], Cl (A 2 "") [] []]) `shouldBe` IntLP [A 1 "", A 3 "", A 2 ""] []
 
         it "upArrow returns model for acceptable LP (first item)" $
-            head (upArrow [(A 1 [], [A 2 []], []), (A 1 [], [A 3 []], []), (A 3 [], [], []), (A 2 [], [], [])]) `shouldBe` [A 1 [], A 3 [], A 2 []]
+            head (upArrow [Cl (A 1 "") [A 2 ""] [A 1 ""], Cl (A 1 "") [A 1 ""] [], Cl (A 3 "") [] [], Cl (A 2 "") [] []]) `shouldBe` IntLP [A 3 "", A 2 ""] [A 1 ""]
 
-    describe "Tests for functions in Completion module" $ do
-        it "trueD' returns the value of disjunction, where one of the elements is T" $
-            trueD' (D [C [V (A 1 []), N (V (A 2 []))], T]) ([V (A 1 [])], []) `shouldBe` Tr
+    --describe "Tests for functions in CPL module" $ do
+      --  it "eval2v returns the value of disjunction, where one of the elements is T" $
+        --    eval2v (D [C [V (A 1 ""), N (V (A 2 ""))], T]) ([V (A 1 "")], []) `shouldBe` Tr
