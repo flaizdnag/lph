30215b7915d403a07bee7d45f1ec0539ce4f1f04
flaizdnag <andrzej.m.gajda@gmail.com>
2018-12-10 11:41:42 +0100

    FO report v1.1

--- src/FindOut.hs
+++ src/FindOut.hs
@@ -65,7 +65,7 @@ getCols = do
     string "KOLUMNA 1" <|> string "KOLUMNA 2"
     eol
     item <- many1 getItem
-    count 2 eol
+    count 1 eol
     return item
 
 getSituations :: ReadP [String]
